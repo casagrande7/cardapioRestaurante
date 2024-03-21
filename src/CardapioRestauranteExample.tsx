2b95e8531115bcaa35e5017c2d68ca9a3ee96f9d
casagrande7 <joaofootz10@gmail.com>
2024-03-21 12:52:21 -0300

    Atualizado

--- src/CardapioRestauranteExample.tsx
+++ src/CardapioRestauranteExample.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import {View, Text, StyleSheet, Image, TouchableOpacity, FlatList} from "react-native";
-
+import { View, Text, StyleSheet, Image, TouchableOpacity, FlatList, StatusBar, TextInput,} from "react-native";
+import * as Animatable from 'react-native-animatable';
 interface MenuItem {
     id: string;
     nome: string;
@@ -10,46 +10,77 @@ interface MenuItem {
 }
 
 const dados: MenuItem[] = [
-    {id: '1', nome: 'Hambúrguer', preco: 'R$20,00', descricao: 'Pão de brioche, 2 carnes, alface, tomate, queijo, bacon', 
-    image: require('./assets/imagens/h.png')},
-    {id: '2', nome: 'Hambúrguer', preco: 'R$25,00', descricao: 'Pão de brioche, 2 carnes, alface, tomate, queijo, bacon', 
-    image: require('./assets/imagens/h.png')},
-    {id: '3', nome: 'Hambúrguer', preco: 'R$20,00', descricao: 'Pão de brioche, 2 carnes, alface, tomate, queijo, bacon', 
-    image: require('./assets/imagens/h.png')},
-    {id: '4', nome: 'Hambúrguer', preco: 'R$20,00', descricao: 'Pão de brioche, 2 carnes, alface, tomate, queijo, bacon', 
-    image: require('./assets/imagens/h.png')},
-    {id: '5', nome: 'Hambúrguer', preco: 'R$20,00', descricao: 'Pão de brioche, 2 carnes, alface, tomate, queijo, bacon', 
-    image: require('./assets/imagens/h.png')},
-    {id: '6', nome: 'Hambúrguer', preco: 'R$20,00', descricao: 'Pão de brioche, 2 carnes, alface, tomate, queijo, bacon', 
-    image: require('./assets/imagens/h.png')},
-    {id: '7', nome: 'Hambúrguer', preco: 'R$20,00', descricao: 'Pão de brioche, 2 carnes, alface, tomate, queijo, bacon', 
-    image: require('./assets/imagens/h.png')},
-    {id: '8', nome: 'Hambúrguer', preco: 'R$20,00', descricao: 'Pão de brioche, 2 carnes, alface, tomate, queijo, bacon', 
-    image: require('./assets/imagens/h.png')},
-    {id: '9', nome: 'Hambúrguer', preco: 'R$20,00', descricao: 'Pão de brioche, 2 carnes, alface, tomate, queijo, bacon', 
-    image: require('./assets/imagens/h.png')},
-    {id: '10', nome: 'Hambúrguer', preco: 'R$20,00', descricao: 'Pão de brioche, 2 carnes, alface, tomate, queijo, bacon', 
-    image: require('./assets/imagens/h.png')},
-    {id: '11', nome: 'Hambúrguer', preco: 'R$20,00', descricao: 'Pão de brioche, 2 carnes, alface, tomate, queijo, bacon', 
-    image: require('./assets/imagens/h.png')},
-    {id: '12', nome: 'Hambúrguer', preco: 'R$20,00', descricao: 'Pão de brioche, 2 carnes, alface, tomate, queijo, bacon', 
-    image: require('./assets/imagens/h.png')},
-    {id: '13', nome: 'Hambúrguer', preco: 'R$20,00', descricao: 'Pão de brioche, 2 carnes, alface, tomate, queijo, bacon', 
-    image: require('./assets/imagens/h.png')},
-    {id: '14', nome: 'Hambúrguer', preco: 'R$20,00', descricao: 'Pão de brioche, 2 carnes, alface, tomate, queijo, bacon', 
-    image: require('./assets/imagens/h.png')},
-    {id: '15', nome: 'Hambúrguer', preco: 'R$20,00', descricao: 'Pão de brioche, 2 carnes, alface, tomate, queijo, bacon', 
-    image: require('./assets/imagens/h.png')},
+    {
+        id: '1', nome: 'Hambúrguer: X BACON', preco: 'R$20,00', descricao: 'Pão de brioche, 2 carnes, alface, tomate, queijo, bacon.',
+        image: require('./assets/imagens/h.png')
+    },
+    {
+        id: '2', nome: 'Hambúrguer: ', preco: 'R$20,00', descricao: 'Pão de brioche, 2 carnes, alface, tomate, queijo, bacon',
+        image: require('./assets/imagens/h.png')
+    },
+    {
+        id: '3', nome: 'Hambúrguer: ', preco: 'R$20,00', descricao: 'Pão de brioche, 2 carnes, alface, tomate, queijo, bacon',
+        image: require('./assets/imagens/h.png')
+    },
+    {
+        id: '4', nome: 'Hambúrguer: ', preco: 'R$20,00', descricao: 'Pão de brioche, 2 carnes, alface, tomate, queijo, bacon',
+        image: require('./assets/imagens/h.png')
+    },
+    {
+        id: '5', nome: 'Hambúrguer: ', preco: 'R$20,00', descricao: 'Pão de brioche, 2 carnes, alface, tomate, queijo, bacon',
+        image: require('./assets/imagens/h.png')
+    },
+    {
+        id: '6', nome: 'Hambúrguer: ', preco: 'R$20,00', descricao: 'Pão de brioche, 2 carnes, alface, tomate, queijo, bacon',
+        image: require('./assets/imagens/h.png')
+    },
+    {
+        id: '7', nome: 'Hambúrguer: ', preco: 'R$20,00', descricao: 'Pão de brioche, 2 carnes, alface, tomate, queijo, bacon',
+        image: require('./assets/imagens/h.png')
+    },
+    {
+        id: '8', nome: 'Hambúrguer: ', preco: 'R$20,00', descricao: 'Pão de brioche, 2 carnes, alface, tomate, queijo, bacon',
+        image: require('./assets/imagens/h.png')
+    },
+    {
+        id: '9', nome: 'Hambúrguer: ', preco: 'R$20,00', descricao: 'Pão de brioche, 2 carnes, alface, tomate, queijo, bacon',
+        image: require('./assets/imagens/h.png')
+    },
+    {
+        id: '10', nome: 'Hambúrguer: ', preco: 'R$20,00', descricao: 'Pão de brioche, 2 carnes, alface, tomate, queijo, bacon',
+        image: require('./assets/imagens/h.png')
+    },
+    {
+        id: '11', nome: 'Hambúrguer: ', preco: 'R$20,00', descricao: 'Pão de brioche, 2 carnes, alface, tomate, queijo, bacon',
+        image: require('./assets/imagens/h.png')
+    },
+    {
+        id: '12', nome: 'Hambúrguer: ', preco: 'R$20,00', descricao: 'Pão de brioche, 2 carnes, alface, tomate, queijo, bacon',
+        image: require('./assets/imagens/h.png')
+    },
+    {
+        id: '13', nome: 'Hambúrguer: ', preco: 'R$20,00', descricao: 'Pão de brioche, 2 carnes, alface, tomate, queijo, bacon',
+        image: require('./assets/imagens/h.png')
+    },
+    {
+        id: '14', nome: 'Hambúrguer: ', preco: 'R$20,00', descricao: 'Pão de brioche, 2 carnes, alface, tomate, queijo, bacon',
+        image: require('./assets/imagens/h.png')
+    },
+    {
+        id: '15', nome: 'Hambúrguer: ', preco: 'R$20,00', descricao: 'Pão de brioche, 2 carnes, alface, tomate, queijo, bacon',
+        image: require('./assets/imagens/h.png')
+    },
 ];
 
-const renderMenuItem = ({item}: {item: MenuItem}) => (
+const renderMenuItem = ({ item }: { item: MenuItem }) => (
     <TouchableOpacity style={styles.item}>
-        <Text style={styles.text}>{item.nome}</Text>
-        <View style={styles.espacoDescricao}>
-        <Image source={item.image} style={styles.imagemH}></Image>
+        <Text style={styles.textNome}>{item.nome}</Text>
         <Text style={styles.text}>{item.descricao}</Text>
-        <Text style={styles.text}>{item.preco}</Text>
-        </View>
+        <Text style={styles.textPreco}>{item.preco}</Text>
+        <Image source={item.image} style={styles.imagemH}></Image>
+        <TouchableOpacity>
+            <Image source={require('./assets/imagens/mais.png')} style={styles.maisImagem}></Image>
+        </TouchableOpacity>
     </TouchableOpacity>
 
 );
@@ -57,29 +88,32 @@ const renderMenuItem = ({item}: {item: MenuItem}) => (
 function CardapioRestauranteExample(): React.JSX.Element {
     return (
         <View style={styles.container}>
-            <View style={styles.header}>
-                <Text style={styles.headerText}>Ifood</Text>
-            </View>
+            <StatusBar backgroundColor={'#D2B48C'}></StatusBar>
+            <Animatable.View  animation={'fadeInDown'} delay={30} style={styles.header}>
+                <Image source={require('./assets/imagens/hh.png')} style={styles.imagem}></Image>
+                <Text style={styles.headerText}>FOOD KING</Text>
+            </Animatable.View>
+            
             <FlatList
-            showsVerticalScrollIndicator={false}
-            data={dados}
-            renderItem={renderMenuItem}
-            keyExtractor={(item) => item.id}
+                showsVerticalScrollIndicator={false}
+                data={dados}
+                renderItem={renderMenuItem}
+                keyExtractor={(item) => item.id}
             />
-            <View style={styles.footer}>
-            <TouchableOpacity>
+            <Animatable.View animation={'fadeInUp'} delay={30} style={styles.footer}>
+                <TouchableOpacity>
                     <Image source={require('./assets/imagens/i.png')} style={styles.footerIcon} />
                 </TouchableOpacity>
                 <TouchableOpacity>
                     <Image source={require('./assets/imagens/ll.png')} style={styles.footerIcon} />
                 </TouchableOpacity>
                 <TouchableOpacity>
-                    <Image source={require('./assets/imagens/p.png')} style={styles.footerIcon}/>
+                    <Image source={require('./assets/imagens/p.png')} style={styles.footerIcon} />
                 </TouchableOpacity>
                 <TouchableOpacity>
-                    <Image source={require('./assets/imagens/e.png')} style={styles.footerIcon}/>
+                    <Image source={require('./assets/imagens/cardapio.png')} style={styles.footerIcon} />
                 </TouchableOpacity>
-            </View>
+            </Animatable.View>
         </View>
 
     );
@@ -94,25 +128,32 @@ const styles = StyleSheet.create({
         padding: 20,
         marginVertical: 8,
         marginHorizontal: 16,
-        borderRadius: 15
+        borderRadius: 15,
+        borderWidth: 2,
+        borderColor: 'white'
 
     },
     header: {
         backgroundColor: '#4d663a',
         alignItems: 'center',
-        paddingVertical: 10
-
+        paddingVertical: 10,
+        borderBottomLeftRadius: 20,
+        borderBottomRightRadius: 20
     },
     headerText: {
         fontSize: 20,
         fontWeight: 'bold',
-        color: 'white'
+        color: 'white',
+        marginTop: -40
+
 
     },
     imagemH: {
         height: 100,
         width: 100,
-        
+        marginTop: -100,
+        marginLeft: 200
+
     },
     footer: {
         borderTopWidth: 0.2,
@@ -120,7 +161,8 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-around',
         alignItems: 'center',
-        paddingVertical: 10
+        paddingVertical: 10,
+        borderRadius: 12
 
     },
     footerIcon: {
@@ -130,13 +172,57 @@ const styles = StyleSheet.create({
     text: {
         fontWeight: 'bold',
         fontSize: 15,
-        
+        textAlign: 'justify',
+        marginRight: 140,
     },
-    espacoDescricao:{
-        flexDirection: 'column',
+    maisImagem: {
+        width: 30,
+        height: 30,
+        marginTop: 4
+    },
+    textPreco: {
+        fontWeight: 'bold',
+        fontSize: 20,
+        textAlign: 'center',
+        marginRight: 170,
+        color: 'white',
+        padding: 5,
+        marginVertical: 8,
+        marginHorizontal: 16,
+        borderRadius: 15,
+        borderWidth: 2,
+        borderColor: 'white',
+        marginLeft: 0
+    },
+    imagem: {
+        width: 50,
+        height: 50,
+        marginRight: 300,
+        marginTop: -8
+    },
+    textMenu: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'white',
+        marginLeft: 240,
+        marginTop: -27
+
+    },
+    imagemMenu: {
+        marginLeft: 215,
+        height: 20,
+        width: 20,
+        marginTop: -20
+    },
+    textNome: {
+        fontWeight: 'bold',
+        fontSize: 20,
+        textAlign: 'justify',
+        marginRight: 40,
+        textDecorationLine: 'underline',
+        lineHeight: 25,
     }
-    
-    
+
 });
 
 export default CardapioRestauranteExample;
